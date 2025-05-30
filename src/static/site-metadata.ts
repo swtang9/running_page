105088ed0ac2d46ec2e15a0856810b763fd0b9c7
Shaowu Tang (唐绍武) <swtang918@gmail.com>
2025-05-30 20:37:30 +0800

    running

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,22 +15,22 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteTitle: 'Running Page HHH',
+  siteUrl: 'https://shaowutang.com/',
+  logo: 'https://github.com/swtang9/swtang9.github.io/blob/master/images/android-chrome-192x192.png',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Summary',
       url: `${getBasePath()}/summary`,
     },
+    // {
+    //   // name: 'Blog',
+    //   // url: 'https://shaowutang.com/',
+    // },
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: 'Shaowu Tang',
+      url: 'https://shaowutang.com/',
     },
   ],
 };
